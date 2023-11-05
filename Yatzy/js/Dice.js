bada8ac5922a405fd3c994cd7583c399e3bfa387
Rock Parker <77560907+RockParker@users.noreply.github.com>
2023-11-05 16:07:02 -0500

    Update Dice.js
    added comments

--- Yatzy/js/Dice.js
+++ Yatzy/js/Dice.js
@@ -7,6 +7,8 @@ class Dice
         this.number = number;
         this.isActive = true;
 
+        //this number is the base amount that the image element representing the dice will be rotated at.
+        //more info in the animate method
         this.seed = 120 + Math.floor(Math.random() * 240);
 
         this.imgEl = document.getElementById(element);
@@ -52,6 +54,9 @@ class Dice
         }
     }
 
+    /** 
+    * sets the dice to a "neutral" state to make sure that it is ready to be used again
+    */
     reset()
     {
         this.isActive = true;
@@ -62,13 +67,22 @@ class Dice
         this.imgEl.style.transform = 'rotate(0deg)';
     }
 
+    /**
+    *sets the image that should be showing based on the rolled number
+    */
     #setImg()//private
     {
         this.imgEl.setAttribute('src', 'Dice/'+this.number+'_face.png');
     }
 
+    /**
+        rotates the image element using css, giving us our animation
+    */
     #animateDice()
     {
+        //uses a instance variable to make sure that the dice will always roll a minimum amount
+        //it takes previous number, then adds the seed amount, and sets the number lower than 360
+        //this step also makes it so that the dice sometimes roll the other direction, which is nice
         this.prevRotate = (this.prevRotate += this.seed) % 360;
 
         this.imgEl.style.transform = 'rotate('+ this.prevRotate+ 'deg)';
